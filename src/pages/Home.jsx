08dfcb514c05ede02d8cb420248b77b0b7b5505a
anushgs <anush.gs@mca.christuniversity.in>
2022-06-28 00:06:48 +0530

    try

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import IotDashboard from "../components/IotDashboard";
+
+const Home = () => {
+  return (
+    <>
+      <IotDashboard />
+    </>
+  );
+};
+
+export default Home;
